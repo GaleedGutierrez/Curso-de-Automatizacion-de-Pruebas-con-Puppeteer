e1c2d84b95368086bc02b7e740cacef1d4a8c582
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-03-13 18:44:45 -0300

    feat: :white_check_mark: Clase N°6
    Visual Testing

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ import type { Config } from 'jest';
 const config: Config = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	clearMocks: true,
+	// clearMocks: true,
 	// collectCoverage: true,
 	// coverageDirectory: 'coverage',
 	// coverageProvider: 'v8',
